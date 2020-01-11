9edc4d5c250c9ff1b6bd1748fe8eea99b4f1b956
mateu <mateusz.bialos@gmail.com>
2020-01-11 14:03:51 +0100

    Initial commit

---
+++ src/main/java/pl/mateusz/forothers/laundry_karol/LaundryKarolApplication.java
@@ -0,0 +1,13 @@
+package pl.mateusz.forothers.laundry_karol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaundryKarolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaundryKarolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/mateusz/forothers/laundry_karol/LaundryKarolApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mateusz.forothers.laundry_karol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaundryKarolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
